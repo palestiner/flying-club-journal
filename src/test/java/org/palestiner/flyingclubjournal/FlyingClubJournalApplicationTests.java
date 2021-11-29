eb09a879d6c72c0a076667b74c53e97a11bc0921
Yuri Medvedev <megapalestiner@gmail.com>
2021-11-29 13:41:56 +0700

    init

---
+++ src/test/java/org/palestiner/flyingclubjournal/FlyingClubJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.palestiner.flyingclubjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingClubJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
